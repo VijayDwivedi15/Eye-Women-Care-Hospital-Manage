c1016034748b209700e27bff1b9cf4aa5f734894
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2020-06-22 12:31:00 +0530

    Add project files.

---
+++ SaiBabaHospital/Controllers/HomeEyeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaiBabaHospital.Controllers
+{
+    public class HomeEyeController : Controller
+    {
+        //
+        // GET: /HomeEye/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Doctors()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUsRaipur()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            return View();
+        }
+
+        public ActionResult Media()
+        {
+            return View();
+        }
+
+        public ActionResult Career()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ SaiBabaHospital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SaiBabaHospital.MvcApplication" Language="C#" %>
